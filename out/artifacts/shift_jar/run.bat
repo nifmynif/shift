56f839d2045e4880083c50f7b5a390262b4e97ce
nifmynif <nifmynif@gmail.com>
2025-01-31 06:59:39 +0300

    Мелкие исправления

--- out/artifacts/shift_jar/run.bat
+++ out/artifacts/shift_jar/run.bat
@@ -1,4 +1,5 @@
 @echo off
 chcp 65001
-java -Dfile.encoding=UTF-8 -jar shift.jar --input=in.txt --sort=name --order=asc
+java -Dfile.encoding=UTF-8 -jar shift.jar --sort=name --order=asc --output=file --path=out.txt
+java -Dfile.encoding=UTF-8 -jar shift.jar --input=in.txt --sort=salary --order=desc
 pause
